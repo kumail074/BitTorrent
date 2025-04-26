e7cd383af2a33a43349aa5dbcc4bab5bb6fd19aa
Kumail Haider <kumail074@proton.me>
2025-04-26 17:01:24 +0500

    Project

--- Torrent/Class1.cs
+++ Torrent/Class1.cs
@@ -1,6 +1,21 @@
-﻿namespace Torrent;
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
+namespace Torrent
+{
+    public static class BEncoding
+    {
+        private static byte DictionaryStart = System.Text.Encoding.UTF8.GetBytes("d")[0];
+        private static byte DictionaryEnd = System.Text.Encoding.UTF8.GetBytes("e")[0];
+    }
+}
+    
+    
+    
+    
+    
 public class Class1
 {
-
 }
