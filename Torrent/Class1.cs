da76fc7f97fc625c0f9e8291330d2376045261ec
Kumail Haider <kumail074@proton.me>
2025-04-27 00:22:30 +0500

    DecodingMethod

--- Torrent/Class1.cs
+++ Torrent/Class1.cs
@@ -9,6 +9,18 @@ namespace Torrent
     {
         private static byte DictionaryStart = System.Text.Encoding.UTF8.GetBytes("d")[0];
         private static byte DictionaryEnd = System.Text.Encoding.UTF8.GetBytes("e")[0];
+        private static byte ListStart = System.Text.Encoding.UTF8.GetBytes("l")[0];
+        private static byte ListEnd = System.Text.Encoding.UTF8.GetBytes("e")[0];
+        private static byte NumberStart = System.Text.Encoding.UTF8.GetBytes("i")[0];
+        private static byte NumberEnd = System.Text.Encoding.UTF8.GetBytes("e")[0];
+        private static byte ByteArrayDivider = System.Text.Encoding.UTF8.GetBytes(":")[0];
+
+        public static object Decode(byte[] bytes)
+        {
+            IEnumerator<byte> enumerator = ((IEnumerable<byte>)bytes).GetEnumerator();
+            enumerator.MoveNext();
+            return DecodeNextObject(enumerator);
+        }
     }
 }
     
