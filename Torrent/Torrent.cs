8c9ceb8ea797f888bb41928069ddc278492ed62b
Kumail Haider <kumail074@proton.me>
2025-04-28 02:44:44 +0500

    BitTorrent namespace initialized and file created

---
+++ Torrent/Torrent.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace BitTorrent
+{
+    public class Torrent
+    {
+        
+    }
+}
